ae59d1feadb7ae5512454053f1ddc14769af493d
Rsalem <salemrachad@gmail.com>
2020-08-02 15:58:08 +0200

    new pcb design and filtered stl files

---
+++ Breath_Device/Breath_Device_Code/Breath_Attiny85_2aug/Breath_Attiny85_2aug.ino
@@ -0,0 +1,125 @@
+//MICROPHONE SETUP
+
+
+int soundPin = 2;
+
+//int soundPin = A0;
+
+#define MIC_THOLD 5
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// LDR SETUP
+
+//Attiny Pin
+int sensorPin = 3;
+//int sensorPin = A1;
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+//Variables to handle timing for LED
+
+float timerSensor = 0;
+float timeSensor = 600;
+
+//Floor mic value
+unsigned int mic_floor;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// LED SETUP
+//Attiny Pin
+int led = 4;
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 3;    // how many points to fade the LED by
+
+int gstate = 0;
+
+//Variables to handle timing for LED
+float timerLed = 0;
+float timeLed = 2000;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+int period = 500;
+unsigned long time_now = 0;
+
+void get_mic_floor() {
+  unsigned long sum_readings = 0;
+  for (int i = 0; i < 10; i++) {
+    sum_readings += analogRead(soundPin);
+    delay(10);
+  }
+  mic_floor = sum_readings / 10;
+
+}
+
+
+void setup() {
+
+
+  pinMode(led, OUTPUT);
+  get_mic_floor();
+  // Store actual time in every timer
+  timerSensor = millis();
+  timerLed = millis();
+}
+
+void loop() {
+
+  switch (gstate) {
+
+    case 0:
+      readMic();
+//      // If enough time has passed (more than timeSensor - 600ms- )
+//      if (millis() - timerSensor >= timeSensor) {
+//        // Get a new time reference in timerSensor (a new timer count is started)
+//        timerSensor = millis();
+//        sensorValue = analogRead(sensorPin); // read the value from the sensor
+//        if (sensorValue > 400) {
+//          gstate = 1;
+//        }
+//      }
+      break;
+    case 1:
+      brightness = brightness + fadeAmount;
+
+      if ( brightness >= 255) {
+        time_now = millis();
+        delay(500);
+
+
+        fadeAmount = -fadeAmount;
+      }
+      if (brightness < 0) {
+        resetSystem();
+      }
+
+      //if (millis() - timerLed >= timeLed) {
+      //timerLed = millis();
+      analogWrite(led, brightness);
+      delay(30);
+      break;
+      //  }
+  }
+}
+void readMic_analog() {
+
+  long sum = 0;
+  for (int i = 0; i < 100; i++)
+  {
+    sum += analogRead(soundPin);
+  }
+  sum = sum / 100;
+  //Serial.println(sum);
+  if  (sum > mic_floor + MIC_THOLD) gstate = 1;
+}
+
+void readMic(){
+  if (digitalRead(soundPin)==HIGH) gstate=1;
+}
+
+void resetSystem() {
+  fadeAmount = 3;
+  gstate = 0;
+  brightness = 0;
+}
--- Breath_Device/Breath_Device_Pcb/Breath_PCB/Breath_PCB.pro
+++ Breath_Device/Breath_Device_Pcb/Breath_PCB/Breath_PCB.pro
@@ -1,4 +1,4 @@
-update=31/07/2020 10:22:53
+update=02/08/2020 12:59:24
 version=1
 last_client=kicad
 [general]
@@ -39,6 +39,7 @@ MinMicroViaDiameter=0.2
 MinMicroViaDrill=0.09999999999999999
 MinHoleToHole=0.25
 TrackWidth1=0.25
+TrackWidth2=0.6
 ViaDiameter1=0.8
 ViaDrill1=0.4
 dPairWidth1=0.2
@@ -247,10 +248,10 @@ dPairWidth=0.2
 dPairGap=0.25
 dPairViaGap=0.25
 [pcbnew/Netclasses/1]
-Name=0.35
-Clearance=0.4
-TrackWidth=0.35
-ViaDiameter=0.8
+Name=Regular
+Clearance=0.6
+TrackWidth=0.6
+ViaDiameter=1.2
 ViaDrill=0.4
 uViaDiameter=0.3
 uViaDrill=0.1
